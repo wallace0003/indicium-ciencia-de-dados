98c29c43a0b3c355bd8aa5594f8c2f2bff64d4ce
wallace0003 <wallaceizidoro.1@gmail.com>
2025-01-26 16:21:53 -0300

    mudando valores nulos e fazendo uma limpeza dos dados

--- main.py
+++ main.py
@@ -5,4 +5,16 @@ import seaborn as sns
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 
-print("testando")
+df = pd.read_csv("dados/dados_indicium.csv")
+
+# Verificando quantas colunas tem valores nulos
+print(df.isnull().sum())
+#Substituindo valores nulos para melhor análise
+df["nome"].fillna("indefinido", inplace=True)
+df["host_name"].fillna("sem nome", inplace=True)
+df["reviews_por_mes"].fillna(0, inplace=True)
+df["ultima_review"].fillna(0, inplace=True)
+
+print(df.isnull().sum())
+print()
+print(df.dtypes)
