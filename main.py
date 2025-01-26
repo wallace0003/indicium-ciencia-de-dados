8adb73837a1f23ade3de6904f4073610a0824399
wallace0003 <wallaceizidoro.1@gmail.com>
2025-01-26 15:17:36 -0300

    Adicionando bibliotecas e comecando o projeto

---
+++ main.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+
+print("testando")
